5b2ca913ab96b62d1934c916c5d6755f80206208
Tej Pratap <bankebihari5463@gmail.com>
2022-09-02 10:47:51 +0530

    api request function

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-auth',
@@ -9,24 +10,29 @@ import { Router } from '@angular/router';
 })
 export class AuthPage implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private http: HttpClient,private router:Router) { }
 
   ngOnInit() {
 
   }
+  headers= new HttpHeaders()
+      .set('content-type', 'application/json')
+      .set('Access-Control-Allow-Origin', '*');
 
   //login functionality
   isLoggedIn = true;
   login(data:any){
     console.log("button clicked",data);
-    //this.http.post('http://127.0.0.1:8000/user/login',data.value).subscribe(data=>console.log(data))
+    this.http.post('http://127.0.0.1:8000/user/login',data.value,{ 'headers': this.headers }).subscribe(data=>console.log(data))
+
     
-    if(this.isLoggedIn){
-      console.log("user logged in",data.value);
-      this.router.navigate(['/home']);
-    }else{
-      console.log("Invalid credentials");
-    }
+    
+    // if(this.isLoggedIn){
+    //   console.log("user logged in",data.value);
+    //   this.router.navigate(['/home']);
+    // }else{
+    //   console.log("Invalid credentials");
+    // }
   }
 
 }
--- src/app/forget-password/forget-password.page.ts
+++ src/app/forget-password/forget-password.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forget-password',
@@ -7,13 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgetPasswordPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HttpClient,private router:Router) { }
+  users:any=[];
   ngOnInit() {
+    console.log("dashboard init fired");
+    this.getData();
   }
 
   forgetPassword(data:any){
-    console.log("forget password runs")
+    console.log("forget password runs",data.value);
+    
   }
 
-}
+  getData(){
+    this.http.get('http://127.0.0.1:8000/user/getall').subscribe(data=>{
+      this.users=data;
+      console.log(this.users);
+      console.log(this.users.user);
+    });
+  }
+   
+
+};
